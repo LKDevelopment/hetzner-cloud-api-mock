3ec85e9ab1fbdd0bbd16b9ca73521775dc22ed22
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2018-07-19 14:50:45 +0200

    Rename file

--- api-mock-server.sh
+++ api-mock-server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "First get the latest version of the api!\n"
-curl https://docs.hetzner.cloud/docs.md --output docs.md
+curl https://docs.hetzner.cloud/latest_docs.apib --output latest_docs.apib
 echo "Now we start the server"
-./node_modules/.bin/drakov -f docs.md -p 8080 --disableCORS --stealthmode --public
+./node_modules/.bin/drakov -f latest_docs.apib -p 8080 --disableCORS --stealthmode --public
